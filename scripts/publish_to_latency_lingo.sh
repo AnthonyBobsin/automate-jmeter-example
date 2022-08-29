d1c35ff1fee13cd61c698d86187106e0853430b5
Anthony Bobsin <bobsinj@gmail.com>
2022-08-28 21:28:12 -0400

    use v1.3.1 and fix file reference

--- scripts/publish_to_latency_lingo.sh
+++ scripts/publish_to_latency_lingo.sh
@@ -3,11 +3,11 @@
 # This script is used to publish the test results to Latency Lingo.
 
 # Install Latency Lingo
-curl -L https://github.com/AnthonyBobsin/latency-lingo-cli/releases/download/v1.3.0/latency-lingo-cli_1.3.0_Darwin_x86_64.tar.gz | tar -xz
+curl -L https://github.com/AnthonyBobsin/latency-lingo-cli/releases/download/v1.3.1/latency-lingo-cli_1.3.1_Darwin_x86_64.tar.gz | tar -xz
 
 # Invoke the CLI with our results file
 ./latency-lingo-cli publish \
-  --file result.jtl \
+  --file $WORKSPACE/kpi.jtl \
   --api-key $LATENCY_LINGO_API_KEY \
   --label 'Checkout Flow Jenkins' \
   --env development
