887803c89ee685354f06a8bd0b8ce5b01dd0919a
Anthony Bobsin <bobsinj@gmail.com>
2022-09-05 14:56:24 -0400

    update to use v2.0.0

--- scripts/publish_to_latency_lingo.sh
+++ scripts/publish_to_latency_lingo.sh
@@ -3,7 +3,7 @@
 # This script is used to publish the test results to Latency Lingo.
 
 # Install Latency Lingo
-curl -L https://github.com/AnthonyBobsin/latency-lingo-cli/releases/download/v1.3.1/latency-lingo-cli_1.3.1_Darwin_x86_64.tar.gz | tar -xz
+curl -L https://github.com/AnthonyBobsin/latency-lingo-cli/releases/latest/download/latency-lingo-cli_Darwin_x86_64.tar.gz | tar -xz
 
 # Invoke the CLI with our results file
 ./latency-lingo-cli publish \
